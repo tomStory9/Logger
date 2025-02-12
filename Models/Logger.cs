7fc57c1bb016596116a886a2d8f1193f649871ac
Tom Raineri <tomstory9@gmail.com>
2025-02-12 16:25:16 +0100

    Feat: Logger attribut LogType
    added logType in class Logger with error handling in construct

--- Models/Logger.cs
+++ Models/Logger.cs
@@ -6,8 +6,10 @@ namespace LoggerDLL.Models
     public class Logger
     {
         public string LogPath { get; }
+        public LogType LogType { get; set; }
 
-        public Logger(string logPath)
+
+        public Logger(string logPath , LogType logType)
         {
             if (string.IsNullOrWhiteSpace(logPath))
                 throw new ArgumentException("Log path cannot be null or empty.", nameof(logPath));
@@ -15,7 +17,11 @@ namespace LoggerDLL.Models
             if (!Path.IsPathRooted(logPath)) // Ensure it's an absolute path
                 logPath = Path.Combine(AppContext.BaseDirectory, logPath);
 
+            if (!Enum.IsDefined(typeof(LogTypeEnum), logType))
+                throw new ArgumentException("Invalid log type.", nameof(logType));
+
             LogPath = logPath;
+            LogType = logType;
         }
     }
 }
